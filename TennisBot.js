3ad14288b57edc65d13773660209958e53cda2e5
Martin Jones <martin34581@gmail.com>
2015-03-26 13:22:07 +0000

    Seperating/ organising files
    twd in a seperate script

--- TennisBot.js
+++ TennisBot.js
@@ -1,8 +1,10 @@
+var twd = require('countdowns/twd.js);
+eval(fs.readFileSync('tools.js')+'');
 var config = {
 
-	channels: ["#tvno","#thewalkingdead"],
+	channels: ["#tvno"],
 	server: "irc.freenode.net",
-	botName: "TennisBot"
+	botName: "TestennisBot"
 
 };
 
@@ -26,46 +28,7 @@ bot.addListener('message', function(from, to, message) {
 	
 
 });
-bot.addListener('message', function(from, to, message) {
-	
-		//THE WALKING DEAD
-var end1= new Date('03/30/2015 2:00 AM');
-var _second = 1000;
-var _minute = _second * 60;
-var _hour = _minute * 60;
-var _day = _hour * 24;
 
-	function showRemaining(to){
-	//bot.say(to, "call worked");
-	
-	var now = new Date();
-	var distance = end1 - now;
-	var response= "";
-	if(distance < 0){
-		bot.say(to, "TBC");
-	}else{
-		var days = Math.floor(distance / _day);
-		var hours = Math.floor((distance % _day) / _hour);
-		var minutes = Math.floor((distance % _hour) / _minute);
-		var seconds = Math.floor((distance % _minute) / _second);
-
-		response = days + 'days ';
-		response += hours + 'hrs ';
-		response += minutes + 'mins ';
-		response += seconds + 'secs';
-		bot.say(to, response);
-	}
-
-	}
-
-
-	if( message.indexOf('next')> -1 )
-	{
-		showRemaining(to);
-	}
-	
-
-});
 bot.addListener('message', function(from, to, message) {
 
 	if( message.indexOf('tennisball')> -1 )
